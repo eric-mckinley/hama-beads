d4b1894a6594927749ca3f1bbb4267642e273168
Eric McKinley <mckinley.eric@yahoo.co.uk>
2019-09-22 11:55:08 +0100

    scratch tmp upload

---
+++ Hama/stepper.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+time.sleep(4.0)
+ControlPin = (7,11,13,15)
+
+for pin in ControlPin:
+	GPIO.setup(pin, GPIO.OUT)
+	GPIO.output(pin, 0)
+
+seq = [ [1,0,0,0],
+	[1,1,0,0],
+	[0,1,0,0],
+	[0,1,1,0],
+	[0,0,1,0],
+	[0,0,1,1],
+	[0,0,0,1],
+	[1,0,0,1] ]
+
+for i in range (512) :
+	for halfstep in range(8):
+		for pin in range (4) :
+			GPIO.output(ControlPin[pin], seq[halfstep][pin])
+		 
+		time.sleep(0.002)
+
+GPIO.cleanup()
+	
